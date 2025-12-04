e65b9a2772453317a0e30d81722cb8804319235f
MNA <arnoldnzibuk@gmail.com>
2025-12-04 16:27:49 +0300

    Add authentication

--- src/lib/firebase/serverApp.js
+++ src/lib/firebase/serverApp.js
@@ -1,18 +1,14 @@
-import { initializeApp, cert } from 'firebase-admin/app';
-import { getAuth } from 'firebase-admin/auth';
-
-let firebaseApp;
-
 export async function getAuthenticatedAppForUser() {
-  if (!firebaseApp) {
-    firebaseApp = initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),  
-      }),
-    });
-  }
+  const authIdToken = (await cookies()).get("__session")?.value;
+  const firebaseServerApp = initializeServerApp(
+    initializeApp(),
+    {
+      authIdToken,
+    }
+  );
 
-  return { auth: getAuth(firebaseApp) };
+  const auth = getAuth(firebaseServerApp);
+  await auth.authStateReady();
+
+  return { firebaseServerApp, currentUser: auth.currentUser };
 }
