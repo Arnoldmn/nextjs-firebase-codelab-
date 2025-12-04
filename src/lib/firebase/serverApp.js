d0ef72585120fecee9f40cb062107734fa63092c
MNA <arnoldnzibuk@gmail.com>
2025-12-04 15:55:17 +0300

    Add project to firebase

--- src/lib/firebase/serverApp.js
+++ src/lib/firebase/serverApp.js
@@ -1,14 +1,18 @@
-// enforces that this code can only be called on the server
-// https://nextjs.org/docs/app/building-your-application/rendering/composition-patterns#keeping-server-only-code-out-of-the-client-environment
-import "server-only";
+import { initializeApp, cert } from 'firebase-admin/app';
+import { getAuth } from 'firebase-admin/auth';
 
-import { cookies } from "next/headers";
-import { initializeServerApp, initializeApp } from "firebase/app";
+let firebaseApp;
 
-import { getAuth } from "firebase/auth";
-
-// Returns an authenticated client SDK instance for use in Server Side Rendering
-// and Static Site Generation
 export async function getAuthenticatedAppForUser() {
-  throw new Error("not implemented");
+  if (!firebaseApp) {
+    firebaseApp = initializeApp({
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),  
+      }),
+    });
+  }
+
+  return { auth: getAuth(firebaseApp) };
 }
